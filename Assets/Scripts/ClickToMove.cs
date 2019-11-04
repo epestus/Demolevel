bc6558fcc5c65ee0926ab50f475d3cbdabe18f43
epestus <55587355+epestus@users.noreply.github.com>
2019-11-04 18:48:20 +0100

    asd

---
+++ Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToMove : MonoBehaviour
+{
+
+    public float speed;
+    public CharacterController controller;
+    private Vector3 position;
+ 
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            locatePosition();
+        }
+
+        moveToPosition();
+    }
+
+    void locatePosition()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit, 1000))
+        {
+            position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+            Debug.Log(position);
+        }
+    }
+    void moveToPosition()
+    {
+        //game object is moving
+        if(Vector3.Distance(transform.position, position)>2)
+        {
+
+        
+        Quaternion newRotation = Quaternion.LookRotation(position-transform.position);
+        newRotation.x = 0f;
+        newRotation.z = 0f;
+        transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 10);
+        controller.SimpleMove(transform.forward * speed);
+        GetComponent<Animation> ().CrossFade("run");
+        }
+        //game object is not moving
+        else {
+                GetComponent<Animation> ().CrossFade("idle");
+            }
+    }
+}
---
+++ Assets/Scripts/mob.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mob : MonoBehaviour
+{
+    public float speed;
+    public float range;
+    public CharacterController controller;
+    public Transform player;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!inRange())
+        {
+        chase();
+        }
+        else
+        {
+            GetComponent<Animation> ().CrossFade("idle");
+        }
+    }
+
+    bool inRange()
+    {
+        if(Vector3.Distance(transform.position, player.position)<range)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void chase()
+    {
+        transform.LookAt(player.position);
+       ////GetComponent<Animation> ().CrossFade("run");
+    }
+
+     /// <summary>
+    /// Called every frame while the mouse is over the GUIElement or Collider.
+    /// </summary>
+  
+    
+}
